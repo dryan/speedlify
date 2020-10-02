f94befcff64b0573f56d8d140ef7b3422f33fae9
Dan Ryan <dryan@users.noreply.github.com>
2020-10-01 23:25:53 -0400

    Update and rename sample.js to dryan.js

---
+++ _data/sites/dryan.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "DRyan", // optional, falls back to object key
+	description: "The default sites that get tested",
+	options: {
+		frequency: 60 * 24,
+	},
+	urls: [
+		"https://dryan.com/"
+	]
+};
